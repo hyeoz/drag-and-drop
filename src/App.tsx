178bed03256e2b9f1cb4b72fca3bdb092d37b597
hyeoz <jenny970203@gmail.com>
2022-04-22 21:32:24 +0900

    error fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,75 @@
-import { createGlobalStyle } from "styled-components";
-import ToDoList from "./components/ToDoList";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { useRecoilState } from "recoil";
+import styled from "styled-components";
+import { todoState } from "./atoms";
 
-const GlobalStyle = createGlobalStyle`
-  html, body, div, span, applet, object, iframe,
-  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-  a, abbr, acronym, address, big, cite, code,
-  del, dfn, em, img, ins, kbd, q, s, samp,
-  small, strike, strong, sub, sup, tt, var,
-  b, u, i, center,
-  dl, dt, dd, menu, ol, ul, li,
-  fieldset, form, label, legend,
-  table, caption, tbody, tfoot, thead, tr, th, td,
-  article, aside, canvas, details, embed,
-  figure, figcaption, footer, header, hgroup,
-  main, menu, nav, output, ruby, section, summary,
-  time, mark, audio, video {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    font-size: 100%;
-    font: inherit;
-    vertical-align: baseline;
-  }
-  /* HTML5 display-role reset for older browsers */
-  article, aside, details, figcaption, figure,
-  footer, header, hgroup, main, menu, nav, section {
-    display: block;
-  }
-  /* HTML5 hidden-attribute fix for newer browsers */
-  *[hidden] {
-      display: none;
-  }
-  body {
-    line-height: 1;
-  }
-  menu, ol, ul {
-    list-style: none;
-  }
-  blockquote, q {
-    quotes: none;
-  }
-  blockquote:before, blockquote:after,
-  q:before, q:after {
-    content: '';
-    content: none;
-  }
-  table {
-    border-collapse: collapse;
-    border-spacing: 0;
-  }
-  /* 여기서부터 직접 작성 */
-  * {
-    box-sizing: border-box;
-  }
-  body {
-    font-family: 'Source Sans Pro', sans-serif;
-    background-color: ${(props) => props.theme.bgColor};
-    color: ${(props) => props.theme.textColor};
-    font-weight: 300;
-    line-height: 1.2;
-  }
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+const Boards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  width: 100%;
+`;
+const Board = styled.div`
+  background-color: ${(props) => props.theme.boardColor};
+  padding-top: 30px;
+  padding: 20px 10px;
+  border-radius: 5px;
+  min-height: 200px;
+`;
+const Card = styled.div`
+  background-color: ${(props) => props.theme.cardColor};
+  padding: 10px 10px;
+  border-radius: 5px;
+  margin-bottom: 5px;
 `;
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useRecoilState(todoState);
+  // 드랴그가 끝났을 때 실행되는 함수
+  const onDragEnd = () => {
+    console.log("Drag end");
+  };
   return (
-    <>
-      <GlobalStyle />
-      <ToDoList />
-    </>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Wrapper>
+        <Boards>
+          <Droppable droppableId="one">
+            {/* Droppable, Draggable 의 children 은 함수형태여야 함! */}
+            {/* provided / magic 등으로 불림 */}
+            {/* drag and drop 사용할 때 필용한 props 를 다 적을 필요 없이 설정할 수 있도록 해줌 */}
+            {(magic) => {
+              return (
+                <Board {...magic.droppableProps} ref={magic.innerRef}>
+                  {todos.map((todo, index) => (
+                    <Draggable draggableId={todo} index={index} key={index}>
+                      {(provided) => (
+                        <Card
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          ref={provided.innerRef}
+                        >
+                          {todo}
+                        </Card>
+                      )}
+                    </Draggable>
+                  ))}
+                  {magic.placeholder}
+                </Board>
+              );
+            }}
+          </Droppable>
+        </Boards>
+      </Wrapper>
+    </DragDropContext>
   );
-}
+};
 
 export default App;
